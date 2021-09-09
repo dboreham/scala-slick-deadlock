9574e5d0b3d1d3b82e44cd4376cb6dc820dcc14e
David Boreham <dboreham@carbonhealth.com>
2021-09-09 07:15:13 -0600

    Initial code

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "latest"
 scalaVersion := "2.13.5"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.slick" %% "slick" % "3.3.3",
+  "com.typesafe.slick" %% "slick" % "3.4.0-LOCAL",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "org.postgresql" % "postgresql" % "42.2.2",
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -8,30 +8,6 @@ import slick.jdbc.PostgresProfile.api._
 import com.typesafe.scalalogging.Logger
 import slick.jdbc.SQLActionBuilder
 
-object keepForLater {
-
-  def keepForLater(): Unit = {
-
-    def f0(x: String): Future[Unit] = {
-      System.err.print("f0:");
-      f1(x)
-    }
-
-    def f1(x: String): Future[Unit] =
-      Future {
-        throw new RuntimeException("Blam!");
-      }
-    val f = f0("222")
-    try {
-      val r = Await.result(f, 10 seconds)
-    } catch {
-      // will print with f0 when agent is enabled
-      case ex: Throwable => ex.printStackTrace
-    }
-
-  }
-}
-
 object Main extends App {
 
   val logger = Logger("example")
@@ -44,7 +20,9 @@ object Main extends App {
 
   def runConstantDBPings() = {
     val pingQuery =
-      sql"select 1".as[Option[String]].map { x => logger.info("Received ping") }
+      sql"select 1".as[Option[String]].map { 
+        x => logger.info("Received ping") 
+      }
     val pingFuture = Future {
       while (true) {
         logger.info("Sending ping")
@@ -63,54 +41,65 @@ object Main extends App {
   // This shows us whether slick is alive and working
   runConstantDBPings()
 
-  // Query that takes 1 second to execute and returns the current timestamp (as of the beginning of the transaction)
-  // Note: strange passing of id parameter is because slick provides no way other than string interpolation to 
-  // make a plain sql query.
-  def workQuery(id: Int) =
-    sql"select #$id, trunc(extract(epoch from now())) from (select pg_sleep(1)) as nothing"
-      .as[(Int,Int)]
-      .head
+  if (false) {
+    // Query that takes 1 second to execute and returns the current timestamp (as of the beginning of the transaction)
+    // Note: strange passing of id parameter is because slick provides no way other than string interpolation to
+    // make a plain sql query.
+    def workQuery(id: Int) =
+      sql"select #$id, trunc(extract(epoch from now())) from (select pg_sleep(1)) as nothing"
+        .as[(Int, Int)]
+        .head
 
-  val n = 19 // specify concurrency -- if n < database.numThreads then both plain and nested queries will "work"
-            //                        if n >= database.numThreads then the nested queries will starve the pool
-            // If .transactionally is removed from query execution calls below, starvation is not seen until n 
-            // is much larger (19 in the case of the 10-core test machine). This failure mode is also slightl
-            // different in that the database thread pool does not seem to itself starve -- the ping 
-            // queries keep on being sent. There is however no results receiving activity suggesting that
-            // something starved that is needed to receive results.
+    val n =
+      19 // specify concurrency -- if n < database.numThreads then both plain and nested queries will "work"
+    //                        if n >= database.numThreads then the nested queries will starve the pool
+    // If .transactionally is removed from query execution calls below, starvation is not seen until n
+    // is much larger (19 in the case of the 10-core test machine). This failure mode is also slightl
+    // different in that the database thread pool does not seem to itself starve -- the ping
+    // queries keep on being sent. There is however no results receiving activity suggesting that
+    // something starved that is needed to receive results.
 
-  // Execute database transactions that take some time (1 second), log their results
-  def loggingWorkQuery(id: Int) = workQuery(id).map { result => logger.info(s"Work query ${result._1} returned: ${result._2}")}
-  val workTasks = 1 to n map { i =>
-    {
-      logger.info(s"Executing work query ${i}")
-      db.run(loggingWorkQuery(i).transactionally)
+    // Execute database transactions that take some time (1 second), log their results
+    def loggingWorkQuery(id: Int) = workQuery(id).map { result =>
+      logger.info(s"Work query ${result._1} returned: ${result._2}")
     }
-  }
-  // Wait for those to run
-  Await.result(Future.sequence(workTasks), longTime)
-  logger.info("Non-nested queries completed")
-
-  // Execute database transactions that take some time (1 second), log their results
-  // But also execute a second database transaction inside the result function
-  def nestedWorkQuery(id: Int) =  workQuery(id).map { result => logger.info(s"Nested work query ${result._1} returned: ${result._2}")}
-  def evilWorkQuery(id: Int) = workQuery(id).map { result => {
-    val nestedQueryId = id + n
-    logger.info(s"Evil work query ${result._1} returned:  ${result._2}, executing nested work query ${nestedQueryId}")
-    val nestedFuture = db.run(nestedWorkQuery(nestedQueryId))
-    // Wait for its result (needed to starve the connection pool)
-    Await.result(nestedFuture, longTime)
-  }}
-  val evilWorkTasks = 1 to n map { i =>
-    {
-      logger.info(s"Executing evil work query ${i}")
-      db.run(evilWorkQuery(i).transactionally)
+    val workTasks = 1 to n map { i =>
+      {
+        logger.info(s"Executing work query ${i}")
+        db.run(loggingWorkQuery(i).transactionally)
+      }
     }
+    // Wait for those to run
+    Await.result(Future.sequence(workTasks), longTime)
+    logger.info("Non-nested queries completed")
+
+    // Execute database transactions that take some time (1 second), log their results
+    // But also execute a second database transaction inside the result function
+    def nestedWorkQuery(id: Int) = workQuery(id).map { result =>
+      logger.info(s"Nested work query ${result._1} returned: ${result._2}")
+    }
+    def evilWorkQuery(id: Int) = workQuery(id).map { result =>
+      {
+        val nestedQueryId = id + n
+        logger.info(
+          s"Evil work query ${result._1} returned:  ${result._2}, executing nested work query ${nestedQueryId}"
+        )
+        val nestedFuture = db.run(nestedWorkQuery(nestedQueryId))
+        // Wait for its result (needed to starve the connection pool)
+        Await.result(nestedFuture, longTime)
+      }
+    }
+    val evilWorkTasks = 1 to n map { i =>
+      {
+        logger.info(s"Executing evil work query ${i}")
+        db.run(evilWorkQuery(i).transactionally)
+      }
+    }
+    // Wait for those to run
+    Await.result(Future.sequence(evilWorkTasks), longTime)
+    // When the starvation syndrome occurs, we never get to here
+    logger.info("Nested queries completed")
   }
-  // Wait for those to run
-  Await.result(Future.sequence(evilWorkTasks), longTime)
-  // When the starvation syndrome occurs, we never get to here
-  logger.info("Nested queries completed")
   // Sleep for a while longer to allow us to see if the concurrent pings are succeeding
   Thread.sleep(100000)
 
